0fbd826b58285575abded7acb0af67c26d15e8f8
PotatoSpark <reece.francis6@gmail.com>
2026-07-17 18:21:03 +1000

    FireSpark v0.29 — plans in register, Save As, Resources/Library, battery calc, timer alerts, isolation gate, AFSS pack, battery dates, client fade (fs-v30)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fs-v29";
+const CACHE = "fs-v30";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png", "./pdf.min.js", "./pdf.worker.min.js"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
